1eccc843dee2316e3af4547c42e682b06c46ada2
david++ <397157852@qq.com>
2022-08-31 16:09:57 +0800

    SmoothNormalTool

---
+++ HelloUE/Plugins/SmoothNormalTool/Source/SmoothNormalTool/Public/SmoothNormalCommand.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetData.h"
+
+struct NormalInfor
+{
+	FVector normal;
+	float angle;
+};
+class SMOOTHNORMALTOOL_API SmoothNormalCommand
+{
+private:
+	static void SmoothNormalStaticMesh(FAssetData AssetData);
+	static void SmoothNormalSkeletalMesh(FAssetData AssetData);
+	static void SmoothNormalStaticMeshTriangle(FAssetData AssetData);
+public:
+	static void SmoothNormal(TArray<FAssetData> SelectedAssets);
+};
+
+

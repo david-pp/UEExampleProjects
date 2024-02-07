834bb410fc9701910b0fe30515b5e8203ebc2428
davidpp <397157852@qq.com>
2024-02-07 16:00:25 +0800

    Beacon

---
+++ ThirdPersonMP/Source/ThirdPersonMP/Private/Network/MyBeaconPlayerController.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Network/MyBeaconPlayerController.h"
+
+
+// Sets default values
+AMyBeaconPlayerController::AMyBeaconPlayerController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMyBeaconPlayerController::GetSeamlessTravelActorList(bool bToEntry, TArray<AActor*>& ActorList)
+{
+	Super::GetSeamlessTravelActorList(bToEntry, ActorList);
+
+	if (TestBeaconClient)
+	{
+		ActorList.Add(TestBeaconClient);
+	}
+}
+
+
---
+++ ThirdPersonMP/Source/ThirdPersonMP/Public/Network/MyBeaconPlayerController.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyTestBeaconClient.h"
+#include "GameFramework/Actor.h"
+#include "MyBeaconPlayerController.generated.h"
+
+UCLASS()
+class THIRDPERSONMP_API AMyBeaconPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMyBeaconPlayerController();
+	
+	virtual void GetSeamlessTravelActorList(bool bToEntry, TArray<class AActor*>& ActorList) override;
+
+	UPROPERTY(Transient, BlueprintReadWrite, EditAnywhere)
+	AMyTestBeaconClient* TestBeaconClient;
+};

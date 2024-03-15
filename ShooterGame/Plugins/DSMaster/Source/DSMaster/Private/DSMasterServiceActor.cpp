b7b46ac3fef1ffafbbcc6d2e7dfd82997546d849
davidpp <397157852@qq.com>
2024-03-15 16:40:09 +0800

    ShooterGame - Online RPG

---
+++ ShooterGame/Plugins/DSMaster/Source/DSMaster/Private/DSMasterServiceActor.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DSMasterServiceActor.h"
+
+void ADSMasterServiceActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DSMasterService = MakeShared<FDSMasterService>();
+    if (DSMasterService)
+    {
+    	DSMasterService->InitServer(true);
+    	DSMasterService->StartDSMasterHttpService();
+    	DSMasterService->StartDSMasterServer(GetWorld());
+    }
+}
+
+void ADSMasterServiceActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	
+	if (DSMasterService)
+	{
+		DSMasterService->StopServer();
+		DSMasterService = nullptr;
+	}
+}
---
+++ ShooterGame/Plugins/DSMaster/Source/DSMaster/Public/DSMasterServiceActor.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DSMasterService.h"
+#include "DSMasterServiceActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DSMASTER_API ADSMasterServiceActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+protected:
+	TSharedPtr<FDSMasterService> DSMasterService;
+};
